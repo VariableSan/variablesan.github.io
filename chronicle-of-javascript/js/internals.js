dbe0c78f7bf891cdb4a4886ec5c846faaf210d4c
Nursultan Note <nurgaliev_nursultan@mail.ru>
2019-10-02 20:39:11 +0600

    chronicle-of-js has been added

---
+++ chronicle-of-javascript/js/internals.js
@@ -0,0 +1,19 @@
+function offLinksProperty(){
+	$('a[href = "#"]').on('click', (e) => {
+		e.preventDefault()
+	})
+}
+
+
+function activeHeadBurger(){
+	$('.header__button').on('click', function() {
+		$('.header__burger').toggleClass('header__burger_active')
+		$('.header__items').slideToggle(300)
+	})
+}
+
+function particlesEffect(){
+	particlesJS.load('particles-js', './particles.json', function() {
+	  console.log('callback - particles-.js config loaded');
+	})
+}
---
+++ chronicle-of-javascript/js/main.js
@@ -0,0 +1,6 @@
+// ready to start
+$(() => {
+	offLinksProperty()	
+	activeHeadBurger()
+	particlesEffect()
+})
